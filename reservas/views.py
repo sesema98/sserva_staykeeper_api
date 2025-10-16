09ac46a38ce245f5918de2a3ced0d97057cdacfe
sesema98 <sergioserva14@gmail.com>
2025-10-15 22:56:11 -0500

    Viewsets con busqueda en reservas, n° de habitacion y nombres del cliente

--- reservas/views.py
+++ reservas/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Cliente, Reserva
+from .serializers import ClienteSerializer, ReservaSerializer
 
-# Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["numero_habitacion", "cliente__nombre"]
